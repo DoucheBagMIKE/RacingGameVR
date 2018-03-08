ec44fe4fea766ed29075a3b0e8781841a57343d4
macadamc <macadamchris@gmail.com>
2018-03-08 17:06:35 -0500

    VRMenuTest

---
+++ Assets/ShadyPixelVR/Scripts/Menu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Menu : MonoBehaviour
+{
+    [Tooltip("Object that holds the menu GameObjects as children.")]
+    public GameObject menuHolder;
+    [Tooltip("Object that holds the tab the menu will start on.")]
+    public GameObject startTab;
+    [Tooltip("If menu should be currently active.")]
+    public bool menuState;
+    VRTK_TransformFollow transformFollow;
+
+    public void Start()
+    {
+        transformFollow = GetComponent<VRTK_TransformFollow>();
+
+        if (menuState)
+            OpenMenu();
+        else
+            CloseMenu();
+    }
+
+    public void SetTrackedObject(GameObject objectToTrack)
+    {
+        transformFollow.gameObjectToFollow = objectToTrack;
+    }
+
+    public void OpenMenu()
+    {
+        ShowTab(startTab);
+    }
+
+    public void CloseMenu()
+    {
+        HideTabs();
+    }
+
+    public void HideTabs()
+    {
+        //Loops through and sets all children of the menu to not active.
+        for (int i = 0; i < menuHolder.transform.childCount; i++)
+        {
+            menuHolder.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowTab(GameObject tabGameObject)
+    {
+        //Hides all tabs in the menu.
+        HideTabs();
+
+        //Sets the one tab to active.
+        tabGameObject.SetActive(true);
+    }
+}
---
+++ Assets/ShadyPixelVR/Scripts/MenuToggle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuToggle : MonoBehaviour {
+
+    public VRTK_ControllerEvents controllerEvents;
+    public Menu menuObject;
+
+    public void OnEnable()
+    {
+        controllerEvents.ButtonOnePressed += ControllerEvents_ButtonOnePressed;
+    }
+    public void OnDisable()
+    {
+        controllerEvents.ButtonOnePressed -= ControllerEvents_ButtonOnePressed;
+    }
+
+    private void ControllerEvents_ButtonOnePressed(object sender, ControllerInteractionEventArgs e)
+    {
+        //find the new state of the menu..
+        bool newState = !menuObject.menuState;
+        //..set the menus target state to new state..
+        menuObject.menuState = newState;
+
+        if(newState == true)
+        {
+            //..should the menu open..
+            //..set the tracked object to the controller that hit the button..
+            menuObject.SetTrackedObject(e.controllerReference.scriptAlias);
+            //..open the menu
+            menuObject.OpenMenu();
+        }
+        else
+        {
+            //..should the menu close..
+
+            //..close the menu
+            menuObject.CloseMenu();
+        }
+    }
+}
